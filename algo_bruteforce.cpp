bc8c8cdc202a2baf243619d64ceb1e3f065ef5ff
bipentihexium <bipentihexium@gmail.com>
2022-04-03 17:45:09 +0200

    Test multiple keys

--- algo_bruteforce.cpp
+++ algo_bruteforce.cpp
@@ -32,8 +32,6 @@ OTHER DEALINGS IN THE SOFTWARE.
 #include <string>
 #include <thread>
 
-// TODO: add support for testing multiple keys
-
 // nubmer of search threads - usually used processor cores
 constexpr int no_threads = 2;
 // how much algorithms need to be searched to print the count
@@ -43,18 +41,26 @@ constexpr unsigned searched_count_precision = 1;
 // precision of "...avg search speed Nm/s"
 constexpr unsigned search_time_precision = 2;
 // precision of "...avg search speed Nm/s"
-constexpr unsigned search_speed_precision = 2;
+constexpr unsigned search_speed_precision = 3;
 // string that is searched for when checking if result is message
-constexpr const char *message_string = "DATA(";
-// put your custom data here
-constexpr const char key_custom[] = "DLIHCREHTONMAITUBREHTOMYMSIEHS";
-constexpr const char data_custom[] = "AtniotoMK;hHLt hOT(NSCCiMs  aEeMifpCesul)t: su|'yhlRtsW ;tFpb7t actt lbA  L|c  Ar:SlihA7Eof kh rK;LSPrfdswdrBrHekblUytasxKsnc 7uAROslOyELt SSUC;G Amu snOaACtrutyreebN;TPx thNIteelset;seis;  drs  nDdEpdltueUc And o c dyS7tdTlwFcA TItiePoA:(slpr(ltefKaIeeR  EdN;AvtetEE psottTCTEaCoAa IyYfsnouotsbUti sAkSetL;iui hNpnedped nIhcetx ;ttAeN lsytTnhKtD EeiaexRELityHU'Uux )e UMVr NVhtYiHc  rj Blu;Oe rN e pdsYNHIDo6NeS: D OOAlnn aC TC f So RPoGMsntsan EI LtE anoOiEeP    wDThxdt;CLp)eoEHo arS;wrwgiABeRzwAsue evirjOotiimo;L L( NEwros|oIpNiWeisoin;AiOAit2EQnyUlzsR TWRn rfCAeBEO ks PTW in|sngscthezHt eD)";
+constexpr const char *message_string = "; DATA(";
+// put your data here
+constexpr const char data[] = "AtniotoMK;hHLt hOT(NSCCiMs  aEeMifpCesul)t: su|'yhlRtsW ;tFpb7t actt lbA  L|c  Ar:SlihA7Eof kh rK;LSPrfdswdrBrHekblUytasxKsnc 7uAROslOyELt SSUC;G Amu snOaACtrutyreebN;TPx thNIteelset;seis;  drs  nDdEpdltueUc And o c dyS7tdTlwFcA TItiePoA:(slpr(ltefKaIeeR  EdN;AvtetEE psottTCTEaCoAa IyYfsnouotsbUti sAkSetL;iui hNpnedped nIhcetx ;ttAeN lsytTnhKtD EeiaexRELityHU'Uux )e UMVr NVhtYiHc  rj Blu;Oe rN e pdsYNHIDo6NeS: D OOAlnn aC TC f So RPoGMsntsan EI LtE anoOiEeP    wDThxdt;CLp)eoEHo arS;wrwgiABeRzwAsue evirjOotiimo;L L( NEwros|oIpNiWeisoin;AiOAit2EQnyUlzsR TWRn rfCAeBEO ks PTW in|sngscthezHt eD)";
 // uncomment this line if you want to check for ')' at the end when checking message
 //#define CHECK_CLOSING_PAREN
 // uncomment this line if you want to use HCSTSBSH encryption algorithm instead
 //#define INV_ALGO
 
+// you can change which data is used here
+#define _DATA data
 
+// put your keys here
+constexpr const char *keys[] = { "DLIHCREHTONMAITUBREHTOMYMSIEHS", "SHEISMYMOTHERBUTIAMNOTHERCHILD",
+	"HUMANSCANTSOLVETHISSOBETTERSTOPHERE", "EREHPOTSRETTEBOSSIHTEVLOSTNACSNAMUH",
+	//"STEPMOTHER", "REHTOMPETS", "MATERFAMILIAS", "SAILIMAFRETAM", "MATRIARCH", "HCRAIRTAM",
+};
+
+/*
 constexpr const char data1[] = "Nw;:OPxPo st;AEp fbwpe  idIosEtn TnipeMp H ui;zcE ntcrATsHrhUtxHRW   CCsoo( reEMantTtOafStNOBxaIEtARorYATHa trOBuQtx(NCPshtMUt:NpsvLdA aSeNIrUeeAee rsALs|iCeNhEss;apTbtiA;stlAaer)t tOL7P t: s  NotA SsTttdeE' WL kTv puLDSifANr7oiReiS aeytforoMfctt)dlHOsA n;slDRWYdp rtDssLUS)t;SIkC; ac;oteiVH Wi jfidlR; 7udsRE s uDEtoE|lSeT;LcVTlHCSaAd( hnI  zorkHIcpSEeCoAecenEe UyIlNeot;t Tc eEA  KISKi  H t sDxsttn;MhSUi' KAORNtxotTeAE O spAAeOe ets l OCgBt AetbTeHm)ao|iRIiElt YaDnhtBTlhCGeSwTGbn ncLyFthyO N xdTCDeirnyhstAU T(:SOEs lyTEjMsePup lstkRnnpyndUieIe)rF fr6SttTaHfI;Ne Oh:pAc TiMenE s h)esLsbs roOll VcnwLTO;nhKTsnePmUN;UusHdusDt l B Ho72EyMNuRoy znm dwEs IEiAxtteCrwee MeRen ;iB OstnAtL(NroEtwe| (t:se hyniEdr;iKsnt Ee;ooeSoEdug iu Rd H ddCaLSPC ADiiAYA";
 constexpr const char key2[] = "HUMANSCANTSOLVETHISSOBETTERSTOPHERE";
 constexpr const char data2[] = "at iatutsSfo e tT RSdsnaEcuNtseeLoEnoxxeh tAc oRHS; x iwDTUNen deetHOIECICraP e ;dSol Ot|GiMe: UytAEiYe; BPeb voY;sAc ie)Gesrc H M|eS;MHfuITsLMtWEzesAAt  epldn :Ei  eAseLLfp |ktnpmbslHseue SiicntatoNHCs(Ut Tett;ssw d s Crvs fA u  he yEf so;ndturaUsdeDlCeDAitlr);snn ohO:EAiWjl teax  wow   TaMNSis ncAe : sDx( psNpAEt;lo sBAisuAtdpxYetiR;rKEtnn sCiC  (|tttVtNo yhleswiSA)7SEe'bOnL Bo trLelicHgysn7tudotlrtLEshe tOgttd oipMaUfKr;waN:duEtlurys;RhOf7y nITiontsDaIKTkLeupHtr At ARisRiyDIHOV aHLWkClheICre   ldmtbO )A PTwps R ne AtrUoDT AEsH;Nnozslehh(erDTFOK teAFnn;cRpE ;tOOrORNTMcTdh2E'I(LTnobAphj NcyyAa6Pk NEiSN Ir7CNSYRdt KEo)TexEETiSPUoCeBO zLIUtU;lhPQrCAIi tBWsdM;";
@@ -62,14 +68,13 @@ constexpr const char key3[] = "DLIHCREHTONMAITUBREHTOMYMSIEHS";
 constexpr const char key3r[] = "SHEISMYMOTHERBUTIAMNOTHERCHILD";
 constexpr const char data3[] = "AtniotoMK;hHLt hOT(NSCCiMs  aEeMifpCesul)t: su|'yhlRtsW ;tFpb7t actt lbA  L|c  Ar:SlihA7Eof kh rK;LSPrfdswdrBrHekblUytasxKsnc 7uAROslOyELt SSUC;G Amu snOaACtrutyreebN;TPx thNIteelset;seis;  drs  nDdEpdltueUc And o c dyS7tdTlwFcA TItiePoA:(slpr(ltefKaIeeR  EdN;AvtetEE psottTCTEaCoAa IyYfsnouotsbUti sAkSetL;iui hNpnedped nIhcetx ;ttAeN lsytTnhKtD EeiaexRELityHU'Uux )e UMVr NVhtYiHc  rj Blu;Oe rN e pdsYNHIDo6NeS: D OOAlnn aC TC f So RPoGMsntsan EI LtE anoOiEeP    wDThxdt;CLp)eoEHo arS;wrwgiABeRzwAsue evirjOotiimo;L L( NEwros|oIpNiWeisoin;AiOAit2EQnyUlzsR TWRn rfCAeBEO ks PTW in|sngscthezHt eD)";
 constexpr const char data3r[] = ")De tHzehtcsgns|ni WTP sk OEBeACfr nRWT RszlUynQE2tiAOiA;niosieWiNpIo|sorwEN (L L;omiitoOjrive eusAwzReBAigwrw;Sra oHEoe)pLC;tdxhTDw    PeEiOona EtL IE nastnsMGoPR oS f CT Ca nnlAOO D :SeN6oDIHNYsdp e Nr eO;ulB jr  cHiYthVN rVMU e) xuU'UHytiLERxeaieE DtKhnTtysl NeAtt; xtechIn depdenpNh iui;LteSkAs itUbstouonsfYyI aAoCaETCTttosp EEtetvA;NdE  ReeIaKfetl(rpls(:AoPeitIT AcFwlTdt7Syd c o dnA cUeutldpEdDn  srd  ;sies;tesleetINht xPT;NbeeryturtCAaOns umA G;CUSS tLEyOlsORAu7 cnsKxsatyUlbkeHrBrdwsdfrPSL;Kr hk foE7AhilS:rA  c|L  Abl ttca t7bpFt; WstRlhy'|us :t)luseCpfiMeEa  sMiCCSN(TOh tLHh;KMotointA";
-// you can change which data and key is used here
-#define _DATA data_custom
-#define _KEY key_custom
+*/
 template<int N>
 constexpr int const_strlength(char const (&)[N]) { return N-1; }
+template<typename T, int N>
+constexpr int const_arrlength(T const (&)[N]) { return N; }
 constexpr int datalen = const_strlength(_DATA);
-constexpr int keylen = const_strlength(_KEY);
-
+constexpr int keycount = const_arrlength(keys);
 
 enum class operation {
 	CONSTANT, VAR, REV, ADD, MULT, COUNT
@@ -81,6 +86,7 @@ struct algo_context {
 	int index;
 	int keyindex;
 	std::string d;
+	std::string k;
 };
 class expr {
 public:
@@ -145,9 +151,9 @@ public:
 			case variable::DATA: return c.d[c.index];
 			case variable::DATALEN: return c.d.size();
 			case variable::INDEX: return c.index;
-			case variable::KEY: return _KEY[c.keyindex]-'@';
+			case variable::KEY: return c.k[c.keyindex]-'@';
 			case variable::KEYINDEX: return c.keyindex;
-			case variable::KEYLEN: return keylen;
+			case variable::KEYLEN: return c.k.size();
 			default: return 0;
 			}
 		case operation::REV: return -a->get(c);
@@ -197,11 +203,12 @@ public:
 	 * @brief runs the algorithm on _DATA with _KEY
 	 * @returns resulting message
 	 */
-	std::string run() {
-#ifdef INV_ALGO
+	std::string run(const char *key) {
 		algo_context ctx;
 		ctx.index = start_index;
-		ctx.keyindex = start_keyindex;
+		ctx.k = key;
+		ctx.keyindex = start_keyindex % ctx.k.size();
+#ifdef INV_ALGO
 		std::string res(datalen, '-');
 		const char *c;
 		for (c = _DATA; *c; ++c) {
@@ -225,15 +232,12 @@ public:
 				return res;
 			}
 			ctx.keyindex = keyindex_expr->get(ctx);
-			while (ctx.keyindex < 0) { ctx.keyindex += keylen; }
-			ctx.keyindex %= keylen;
+			while (ctx.keyindex < 0) { ctx.keyindex += ctx.k.size(); }
+			ctx.keyindex %= ctx.k.size();
 			res[ctx.index] = *c;
 		}
 		return res;
 #else
-		algo_context ctx;
-		ctx.index = start_index;
-		ctx.keyindex = start_keyindex;
 		ctx.d = _DATA;
 		std::string res;
 		res.reserve(datalen);
@@ -242,8 +246,8 @@ public:
 			while (ctx.index < 0) { ctx.index += ctx.d.size(); }
 			ctx.index %= ctx.d.size();
 			ctx.keyindex = keyindex_expr->get(ctx);
-			while (ctx.keyindex < 0) { ctx.keyindex += keylen; }
-			ctx.keyindex %= keylen;
+			while (ctx.keyindex < 0) { ctx.keyindex += ctx.k.size(); }
+			ctx.keyindex %= ctx.k.size();
 			res.push_back(ctx.d[ctx.index]);
 			ctx.d.erase(ctx.index, 1);
 		}
@@ -374,9 +378,9 @@ expr *randExpr(RANDOM_T &random_engine, expr *base) {
 template<typename RANDOM_T>
 algo randAlgo(RANDOM_T &random_engine) {
 #ifdef INV_ALGO
-	return algo(R(2) * R(datalen), R(2) * R(keylen), randExpr(random_engine, randLeaf(random_engine)), randExpr(random_engine, new expr(variable::KEYINDEX)));
+	return algo(R(2) * R(datalen), R(2) * R(datalen), randExpr(random_engine, randLeaf(random_engine)), randExpr(random_engine, new expr(variable::KEYINDEX)));
 #else
-	return algo(R(2) * R(datalen), R(2) * R(keylen), randExpr(random_engine, new expr(variable::INDEX)), randExpr(random_engine, new expr(variable::KEYINDEX)));
+	return algo(R(2) * R(datalen), R(2) * R(datalen), randExpr(random_engine, new expr(variable::INDEX)), randExpr(random_engine, new expr(variable::KEYINDEX)));
 #endif
 }
 #undef R
@@ -389,10 +393,12 @@ void runtests() {
 	std::mt19937 random_engine;
 	while (do_search) {
 		algo a = randAlgo(random_engine);
-		std::string res = a.run();
-		if (ismsg(res)) {
-			std::cout << "==================================== FOUND ====================================" << std::endl <<
-				a << std::endl << "-------------- out ------------------" << std::endl << res << std::endl;
+		for (const char * const &key : keys) {
+			std::string res = a.run(key);
+			if (ismsg(res)) {
+				std::cout << "============================== FOUND <KEY: " << key << "> ===============================" << std::endl <<
+					a << std::endl << "-------------- out ------------------" << std::endl << res << std::endl;
+			}
 		}
 		if ((++searched)%searchprint == 0) {
 			std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
